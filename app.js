1ec3f33d822d106ffda8a7c0466e369839369f6e
Vladimir <vladimirkrylov@gmail.com>
2022-03-21 08:53:16 +0300

    @fix Исправлена ошибка при первом посещении сервера в браузере

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cardRouter from "./routes/card.router.js";
-import errorsHandler from "./middlewares/errors.middleware.js";
 import {errors} from "celebrate";
-import NotFoundError from "./errors/not-found.error.js";
 
 const PORT = 5000
 const DB_URL = `mongodb+srv://admin:admin@cluster0.crb4w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
@@ -12,16 +10,13 @@ const app = express()
 app.use(express.json())
 app.use(cardRouter)
 
-app.use((req, res, next) => next(new NotFoundError('Неверный запрос')));
-
 app.use(errors())
-app.use(errorsHandler)
 
 async function startApp() {
 	try {
 		await mongoose.connect(DB_URL)
 		app.listen(PORT, () => {
-			console.log('app started')
+			console.log(`app started on http://localhost:${PORT}`)
 		})
 	} catch (e) {
 		console.log(e)
